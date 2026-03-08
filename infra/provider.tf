b4f578090a2425ef083af5024cfa7dafbc3262db
Saulo Daniel da Silva <saul.osk@hotmail.com>
2026-03-08 18:54:05 -0300

    implementing a k8s service simple with only one node, to deploy the api, using terraform e provisoning a artfactory to it

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.3" # Garante que o código rode com uma versão compatível do Terraform
+
+  backend "gcs" {
+    bucket = "terraform-state-projeto-globo-489614"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
