f2da7a7471da67dbbf87532c847715346252da1e
Saulo Daniel da Silva <saul.osk@hotmail.com>
2026-03-08 23:03:14 -0300

    adding netdata to monitoring and addin probles en rollout to avoid downtime in some update

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,6 +11,14 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
   }
 }
 
